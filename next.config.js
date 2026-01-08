e3556de2a952fdaf5c336327636a02fffc08d7b3
Anis Dai <anisdaii1103@gmail.com>
2026-01-09 00:41:22 +0100

    fix: update Next.js and Netlify config to resolve build error

---
+++ next.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media2.dev.to',
+        pathname: '**',
+      },
+    ],
+  },
+  // Fix for Netlify build issues with Next.js internal files
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+}
