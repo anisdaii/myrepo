fd2bb8022bf850af3e24fe1316c25ebd821697bc
Anis Dai <anisdaii1103@gmail.com>
2026-01-09 13:03:17 +0100

    Initial portfolio deployment

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Anis Dai",
+  profile: '/profile.jpg',
+  designation: "Computer Engineering Graduate",
+  description: "My name is Anis Dai. I am a Computer Engineering graduate, driven by a strong curiosity for artificial intelligence and cybersecurity. I enjoy exploring how intelligent systems can be used to detect, analyze, and prevent digital threats. With a solid technical foundation, strong analytical thinking and a problem-solving mindset. I continuously seek to deepen my knowledge through hands-on projects, research and further academic studies. My goal is to grow in an environment that values learning, innovation, and the responsible use of technology to build secure and intelligent solutions.",
+  email: 'anisdaii1103@gmail.com',
+  phone: '+216 20016557',
+  address: 'Tunis, Tunisia',
+  github: 'https://github.com/anisdaii',
+  facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/anis-daii/',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
+  devUsername: "anisdaii",
+  resume: "/myresume.pdf"
+}
