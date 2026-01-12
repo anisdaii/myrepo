e1a47e1afae176e590517b2b0103073e3e4bba36
Anis Dai <anisdaii1103@gmail.com>
2026-01-12 21:02:31 +0100

    chore: final commit

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,11 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Anis Dai - Computer Engineer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Anis Dai. I am a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
   icons: {
-    icon: '/4.svg',
+    icon: 'public/4.png',
   },
 };
 
